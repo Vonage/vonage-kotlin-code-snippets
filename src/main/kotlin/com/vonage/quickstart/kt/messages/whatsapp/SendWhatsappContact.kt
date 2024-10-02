826a40705d6754407f6b5639b7f5426d29259d6c
Sina Madani <sina.madani@vonage.com>
2024-10-02 12:58:02 +0100

    Add WhatsApp custom/template messages

---
+++ src/main/kotlin/com/vonage/quickstart/kt/messages/whatsapp/SendWhatsappContact.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2024 Vonage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.vonage.quickstart.kt.messages.whatsapp
+
+import com.vonage.client.kt.*
+import com.vonage.client.messages.whatsapp.*
+import com.vonage.quickstart.kt.*
+
+fun main() {
+    val client = Vonage {
+        applicationId(VONAGE_APPLICATION_ID)
+        privateKeyPath(VONAGE_APPLICATION_PRIVATE_KEY_PATH)
+    }
+
+    val messageId = client.messages.send(
+        whatsappCustom {
+            to(TO_NUMBER)
+            from(VONAGE_WHATSAPP_NUMBER)
+            custom(mapOf(
+                "type" to "contacts",
+                "contacts" to listOf(
+                    mapOf(
+                        "addresses" to listOf(
+                            mapOf(
+                                "city" to "Menlo Park",
+                                "country" to "United States",
+                                "state" to "CA",
+                                "country_code" to "us",
+                                "street" to "1 Hacker Way",
+                                "type" to "HOME",
+                                "zip" to "94025"
+                            ),
+                            mapOf(
+                                "city" to "Menlo Park",
+                                "country" to "United States",
+                                "state" to "CA",
+                                "country_code" to "us",
+                                "street" to "200 Jefferson Dr",
+                                "type" to "WORK",
+                                "zip" to "94025"
+                            )
+                        ),
+                        "birthday" to "2012-08-18",
+                        "emails" to listOf(
+                            mapOf(
+                                "email" to "test@fb.com",
+                                "type" to "WORK"
+                            ),
+                            mapOf(
+                                "email" to "test@whatsapp.com",
+                                "type" to "WORK"
+                            )
+                        ),
+                        "name" to mapOf(
+                            "first_name" to "Jayden",
+                            "last_name" to "Smith",
+                            "formatted_name" to "J. Smith"
+                        ),
+                        "org" to mapOf(
+                            "company" to "WhatsApp",
+                            "department" to "Design",
+                            "title" to "Manager"
+                        ),
+                        "phones" to listOf(
+                            mapOf(
+                                "phone" to "+1 (940) 555-1234",
+                                "type" to "HOME"
+                            ),
+                            mapOf(
+                                "phone" to "+1 (650) 555-1234",
+                                "type" to "WORK",
+                                "wa_id" to "16505551234"
+                            )
+                        ),
+                        "urls" to listOf(
+                            mapOf(
+                                "url" to "https://www.facebook.com",
+                                "type" to "WORK"
+                            )
+                        )
+                    )
+                )
+            ))
+        }
+    )
+}
---
+++ src/main/kotlin/com/vonage/quickstart/kt/messages/whatsapp/SendWhatsappLinkButton.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2024 Vonage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.vonage.quickstart.kt.messages.whatsapp
+
+import com.vonage.client.kt.*
+import com.vonage.client.common.MessageType
+import com.vonage.client.messages.whatsapp.*
+import com.vonage.quickstart.kt.*
+
+fun main() {
+    val client = Vonage {
+        applicationId(VONAGE_APPLICATION_ID)
+        privateKeyPath(VONAGE_APPLICATION_PRIVATE_KEY_PATH)
+    }
+
+    val messageId = client.messages.send(
+        whatsappCustom {
+            to(TO_NUMBER)
+            from(VONAGE_WHATSAPP_NUMBER)
+            custom(mapOf(
+                "type" to MessageType.TEMPLATE,
+                "template" to mapOf(
+                    "namespace" to WHATSAPP_TEMPLATE_NAMESPACE,
+                    "name" to WHATSAPP_TEMPLATE_NAME,
+                    "language" to mapOf(
+                        "code" to Locale.ENGLISH,
+                        "policy" to Policy.DETERMINISTIC
+                    ),
+                    "components" to listOf(
+                        mapOf(
+                            "type" to "header",
+                            "parameters" to listOf(
+                                mapOf(
+                                    "type" to MessageType.IMAGE,
+                                    "image" to mapOf(
+                                        "link" to HEADER_IMAGE_URL
+                                    )
+                                )
+                            )
+                        ),
+                        mapOf(
+                            "type" to "body",
+                            "parameters" to listOf(
+                                mapOf(
+                                    "type" to MessageType.TEXT,
+                                    "text" to "Anand"
+                                ),
+                                mapOf(
+                                    "type" to MessageType.TEXT,
+                                    "text" to "Quest"
+                                ),
+                                mapOf(
+                                    "type" to MessageType.TEXT,
+                                    "text" to "113-0921387"
+                                ),
+                                mapOf(
+                                    "type" to MessageType.TEXT,
+                                    "text" to "23rd Nov 2019"
+                                )
+                            )
+                        ),
+                        mapOf(
+                            "type" to MessageType.BUTTON,
+                            "index" to 0,
+                            "sub_type" to "url",
+                            "parameters" to listOf(
+                                mapOf(
+                                    "type" to MessageType.TEXT,
+                                    "text" to "1Z999AA10123456784"
+                                )
+                            )
+                        )
+                    )
+                )
+            ))
+        }
+    )
+}
