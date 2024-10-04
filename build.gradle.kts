e71efe7981378daf22105c3a253531d2bbde50a6
Sina Madani <sina.madani@vonage.com>
2024-10-04 17:13:42 +0100

    Add Messages webhooks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("io.ktor.plugin") version "2.3.12"
 }
 
 repositories {
@@ -9,6 +10,8 @@ repositories {
 
 dependencies {
     implementation("com.vonage:server-sdk-kotlin:1.0.0-RC2")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-serialization-jackson")
 }
 
 kotlin {
---
+++ src/main/kotlin/com/vonage/quickstart/kt/IncomingMessage.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 Vonage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.vonage.quickstart.kt
+
+import com.vonage.client.messages.InboundMessage
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.request.receive
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+
+fun main() {
+    embeddedServer(Netty, port = 8000) {
+        routing {
+            post ("/webhooks/inbound-message") {
+                val messageDetails = call.receive<InboundMessage>()
+                println("Message ID "+messageDetails.getMessageUuid()+" of type " +
+                        messageDetails.getMessageType()+" was sent from " +
+                        messageDetails.getFrom()+" to "+messageDetails.getTo()+" via "+
+                        messageDetails.getChannel()+" at "+messageDetails.getTimestamp()
+                )
+                call.respondText("OK")
+            }
+        }
+    }.start(wait = true)
+}
