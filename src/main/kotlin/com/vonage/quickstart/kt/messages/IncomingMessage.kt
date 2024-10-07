0d2ee924bb1f8f6c4d2f3d0d42cdac8f16b26ec0
Sina Madani <sina.madani@vonage.com>
2024-10-07 13:13:59 +0100

    Move Messages webhooks to correct dir

--- src/main/kotlin/com/vonage/quickstart/kt/messages/IncomingMessage.kt
+++ src/main/kotlin/com/vonage/quickstart/kt/messages/IncomingMessage.kt
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.vonage.quickstart.kt
+package com.vonage.quickstart.kt.messages
 
 import com.vonage.client.messages.InboundMessage
 import io.ktor.server.application.call
