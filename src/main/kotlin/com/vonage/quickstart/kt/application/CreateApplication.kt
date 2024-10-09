ed651ace28555ac718da8afe8b5183b79a439927
Sina Madani <sina.madani@vonage.com>
2024-10-09 11:59:25 +0100

    Add Application snippets

---
+++ src/main/kotlin/com/vonage/quickstart/kt/application/CreateApplication.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2024 Vonage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.vonage.quickstart.kt.application
+
+import com.vonage.client.common.HttpMethod
+import com.vonage.client.kt.*
+import com.vonage.quickstart.kt.*
+
+fun main() {
+    val client = Vonage {
+        apiKey(VONAGE_API_KEY)
+        apiSecret(VONAGE_API_SECRET)
+    }
+
+    val application = client.application.create {
+        name("Code Snippets V2 Application")
+        messages {
+            inbound {
+                address("https://example.com/webhooks/inbound")
+                method(HttpMethod.POST)
+            }
+            status {
+                address("https://example.com/webhooks/status")
+                method(HttpMethod.POST)
+            }
+        }
+        voice {
+            answer {
+                address("https://example.com/webhooks/answer")
+                method(HttpMethod.GET)
+            }
+            fallbackAnswer {
+                address("https://fallback.example.com/webhooks/answer")
+                method(HttpMethod.GET)
+            }
+            event {
+                address("https://example.com/webhooks/event")
+                method(HttpMethod.POST)
+            }
+        }
+        rtc {
+            event {
+                address("https://example.com/webhooks/event")
+                method(HttpMethod.POST)
+            }
+        }
+        verify {
+            status {
+                address("https://example.com/webhooks/verify")
+                method(HttpMethod.POST)
+            }
+        }
+        vbc()
+    }
+}
