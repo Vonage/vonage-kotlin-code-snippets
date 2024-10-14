5d052702abeb58b8193a1943867e994e21990816
Sina Madani <sina.madani@vonage.com>
2024-10-14 14:56:06 +0100

    Add Voice Record/Input snippets

---
+++ src/main/kotlin/com/vonage/quickstart/kt/voice/RecordCallSplitAudio.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2024 Vonage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.vonage.quickstart.kt.voice
+
+import com.vonage.client.kt.connectToPstn
+import com.vonage.client.kt.recordAction
+import com.vonage.client.voice.EventWebhook
+import com.vonage.client.voice.ncco.Ncco
+import com.vonage.client.voice.ncco.SplitRecording
+import com.vonage.quickstart.kt.TO_NUMBER
+import com.vonage.quickstart.kt.VONAGE_NUMBER
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.request.path
+import io.ktor.server.request.receive
+import io.ktor.server.response.header
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+
+fun main() {
+    embeddedServer(Netty, port = 8000) {
+        routing {
+            get("/webhooks/answer") {
+                call.response.header("Content-Type", "application/json")
+                call.respond(
+                    Ncco(
+                        recordAction {
+                            eventUrl(call.request.path().replace("answer", "recordings"))
+                            channels(2)
+                            split(SplitRecording.CONVERSATION)
+                        },
+                        connectToPstn(TO_NUMBER) {
+                            from(VONAGE_NUMBER)
+                        }
+                    ).toJson()
+                )
+            }
+            post("/webhooks/recordings") {
+                val event = EventWebhook.fromJson(call.receive())
+                println("Recording URL: ${event.recordingUrl}")
+                call.respond(204)
+            }
+        }
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/com/vonage/quickstart/kt/voice/RecordConversation.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 Vonage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.vonage.quickstart.kt.voice
+
+import com.vonage.client.kt.conversationAction
+import com.vonage.client.voice.EventWebhook
+import com.vonage.client.voice.ncco.EventMethod
+import com.vonage.client.voice.ncco.Ncco
+import com.vonage.quickstart.kt.CONF_NAME
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.request.path
+import io.ktor.server.request.receive
+import io.ktor.server.response.header
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+
+fun main() {
+    embeddedServer(Netty, port = 8000) {
+        routing {
+            get("/webhooks/answer") {
+                call.response.header("Content-Type", "application/json")
+                call.respond(
+                    Ncco(
+                        conversationAction(CONF_NAME) {
+                            record(true)
+                            eventMethod(EventMethod.POST)
+                            eventUrl(call.request.path().replace("answer", "recordings"))
+                        },
+                    ).toJson()
+                )
+            }
+            post("/webhooks/recordings") {
+                val event = EventWebhook.fromJson(call.receive())
+                println("Recording URL: ${event.recordingUrl}")
+                call.respond(204)
+            }
+        }
+    }.start(wait = true)
+}
